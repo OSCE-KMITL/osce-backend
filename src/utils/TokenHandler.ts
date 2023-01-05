e2d28d011515ee9310f1b4283ed38630d68c09b3
aphisit-ths <aphisit.thu@gmail.com>
2023-01-06 06:35:50 +0700

    [auth] setting up auth context , setting up jwt , setting up authorized middleware ,clean

--- src/utils/TokenHandler.ts
+++ src/utils/TokenHandler.ts
@@ -3,7 +3,7 @@ import { Response } from 'express';
 
 export class TokenHandler {
     static createToken(user_id: string, token_version: number): string {
-        return jwt.sign({ user_id, token_version }, process.env['TOKEN_SECRET']!, { expiresIn: '7d' });
+        return jwt.sign({ user_id, token_version }, process.env['TOKEN_SECRET']!, { expiresIn: process.env['TOKEN_EXPIRES_IN']! });
     }
     static sendTokenToCookie(res: Response, token: string) {
         try {
