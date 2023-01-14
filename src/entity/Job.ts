f198803c68737e89d71174bc9f0e35cec11eaccf
A-Sarayut <sarayutaree@hotmail.com>
2023-01-15 00:37:42 +0700

    Initial simple job entity and CRUD functions

---
+++ src/entity/Job.ts
@@ -0,0 +1,82 @@
+import { Company } from './Company';
+import { Field, ObjectType } from 'type-graphql';
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, UpdateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Job {
+    @PrimaryGeneratedColumn('increment')
+    @Field()
+    id: string;
+
+    @Column({ charset: 'utf8', collation: 'utf8_general_ci' })
+    @Field()
+    job_title: string;
+
+    @Column({ charset: 'utf8', collation: 'utf8_general_ci' })
+    @Field()
+    required_major: string;
+
+    @Column({ charset: 'utf8', collation: 'utf8_general_ci' })
+    @Field()
+    project_topic: string;
+
+    @Column({ charset: 'utf8', collation: 'utf8_general_ci' })
+    @Field()
+    nature_of_work: string;
+
+    @Column({ charset: 'utf8', collation: 'utf8_general_ci' })
+    @Field()
+    required_skills: string;
+
+    @Column()
+    @Field()
+    limit: string;
+
+    @Column({ charset: 'utf8', collation: 'utf8_general_ci' })
+    @Field()
+    welfare: string;
+
+    @Column()
+    @Field()
+    compensation: string;
+
+    @Column({ charset: 'utf8', collation: 'utf8_general_ci' })
+    @Field()
+    coop301_fileurl: string;
+
+    @Field()
+    @CreateDateColumn({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP(6)', name: 'created_at' })
+    createdAt!: Date;
+
+    @Field()
+    @UpdateDateColumn({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP(6)', onUpdate: 'CURRENT_TIMESTAMP(6)', name: 'updated_at' })
+    updatedAt!: Date;
+
+    @Field(() => Company)
+    @ManyToOne(() => Company, (company) => company.company_id)
+    @JoinColumn({ name: 'company_id' })
+    company_id: Promise<Company>;
+
+    constructor(
+        job_title: string,
+        required_major: string,
+        project_topic: string,
+        nature_of_work: string,
+        required_skills: string,
+        limit: string,
+        welfare: string,
+        compensation: string,
+        coop301_fileurl: string
+    ) {
+        this.job_title = job_title;
+        this.required_major = required_major;
+        this.project_topic = project_topic;
+        this.nature_of_work = nature_of_work;
+        this.required_skills = required_skills;
+        this.limit = limit;
+        this.welfare = welfare;
+        this.compensation = compensation;
+        this.coop301_fileurl = coop301_fileurl;
+    }
+}
---
+++ src/modules/job/JobController.ts
@@ -0,0 +1,52 @@
+import { JobInput, UpdateJobInput } from './args/JobInput';
+import { Job } from './../../entity/Job';
+import { JobService } from './JobService';
+import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { Service } from 'typedi';
+import { Announcement } from '../../entity/Announcement';
+import { GetWithKeyInput } from '../../shared/args/GetWithKeyInput';
+import { isAuthenticated } from '../../middleware/isAuthenticated';
+import { useAuthorization } from '../../middleware/useAuthorization';
+import { RoleOption } from '../../shared/types/Roles';
+import { AppContext } from '../../shared/types/context-types';
+
+@Resolver()
+@Service()
+export class JobController {
+    constructor(private readonly job_service: JobService) {}
+
+    @Query(() => [Job], { nullable: 'items' })
+    async getAllJob(): Promise<Job[] | null> {
+        try {
+            return this.job_service.getAllJob();
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    @Query(() => Job, { nullable: true })
+    async getJobById(@Arg('job_id') job_id: string): Promise<Job | null> {
+        return this.job_service.getById(job_id);
+    }
+
+    @UseMiddleware(useAuthorization([RoleOption.COMMITTEE,RoleOption.COMPANY]))
+    @Mutation(() => Job, { nullable: true })
+    async createJob(@Arg('job_info') job_info: JobInput, @Ctx() { req }: AppContext): Promise<Job | null> {
+        const { user_id } = req;
+        return this.job_service.createJob(job_info, user_id!);
+    }
+
+    @UseMiddleware(useAuthorization([RoleOption.COMMITTEE,RoleOption.COMPANY]))
+    @Mutation(() => Job, { nullable: true })
+    async deleteJob(@Arg('job_id') job_id: string, @Ctx() {req}: AppContext): Promise<Job | null> {
+        const { user_id } = req;
+        return this.job_service.deleteJob(job_id,user_id!);
+    }
+
+    @Mutation(() => Job, { nullable: true })
+    async updateJob(@Arg('update_input') update_input: UpdateJobInput, @Ctx() {req}: AppContext): Promise<Job | null> {
+        const { user_id } = req;
+        return this.job_service.updateJob(update_input,user_id!);
+    }
+}
